5eaa6c17b9e0b25b440a425eb3818d4b29dc84ec
vik-kalsi <kalsivik@googlemail.com>
2026-03-16 20:46:57 +0000

    Repo first commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title> {{ $title }} </title>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    <style>
+        [x-cloak] { display: none !important; }
+    </style>
+</head>
+
+<body class="my-4 grid justify-center bg-stone-700 text-white">
+    
+
+
+    <div class="mb-4">
+        <ul class="border-2 flex py-2 justify-center">
+            <li class="px-2"><a class="hover:font-bold" href="/">Homepage</a></li>
+            <li class="px-2"><a class="hover:font-bold" href="/login">Login</a></li>
+            <li class="px-2"><a class="hover:font-bold" href="/register">Register</a></li>
+        </ul>
+    </div>
+    
+
+    <div class="border-2 p-4">
+        {{ $slot }}
+    </div>
+    
+</body>
+</html>
---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+
+
+    <x-slot:title>
+        Dashboard
+    </x-slot>
+
+
+    <h1 class="font-bold text-4xl">Dashboard Page</h1>
+
+
+    <div class="mt-4">
+        <p>You have logged in - Username and Password is correct</p>
+    </div>
+
+    
+</x-layout>
